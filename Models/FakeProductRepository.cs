12cd172b2df54ff864185dfe9e80fd31ac3d8a14
WWStudent <WWStudent@MS-GWSTTF2>
2019-09-13 15:38:04 -0400

    Add project files.

---
+++ Models/FakeProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using myStore.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace SportsStore.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product>
+        {
+            new Product { Name = "Shirts", Price = 15 },
+            new Product { Name = "Shoes", Price = 75 },
+            new Product { Name = "Purses", Price = 50 }
+        }.AsQueryable<Product>();
+    }
+}
